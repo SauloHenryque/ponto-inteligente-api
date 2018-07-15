4c81780295ce9908a2f9f7572fdcaec510bf3e9e
Saulo <saulo_henryque@hotmail.com>
2018-07-14 22:53:25 -0300

    Adicionando anotação para corrigir a class de teste

---
+++ src/main/java/com/estudo/pontointeligente/api/repositories/EmpresaRepository.java
@@ -0,0 +1,14 @@
+package com.estudo.pontointeligente.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.estudo.pontointeligente.api.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	//não bloquea a tabela 
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
